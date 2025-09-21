928968903067fd9066fd9b9c924335baba51fa7b
Antonio Scognamiglio <totoscogna99@live.it>
2025-09-21 19:15:45 +0200

    Added Dockerfile and docker-compose.yml
    Added Dockerfile and docker-compose.yml
    Modified bootstrap data in data.sql

--- src/main/java/com/project/cinemille/controller/CinemaController.java
+++ src/main/java/com/project/cinemille/controller/CinemaController.java
@@ -8,10 +8,7 @@ import com.project.cinemille.service.MovieService;
 import com.project.cinemille.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
