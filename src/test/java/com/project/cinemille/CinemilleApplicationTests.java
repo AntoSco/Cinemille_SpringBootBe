e17434254b34f965096b2faf255df0e843ffbffc
Antonio Scognamiglio <totoscogna99@live.it>
2025-09-21 11:42:55 +0200

    initial commit

---
+++ src/test/java/com/project/cinemille/CinemilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cinemille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
